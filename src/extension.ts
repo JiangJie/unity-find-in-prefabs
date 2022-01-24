d64e4e792bc2c5b6e9a92945280970851a737a44
jarvisjiang <jarvisjiang@tencent.com>
2022-01-25 00:31:03 +0800

    add progress in status bar when building cache

--- src/extension.ts
+++ src/extension.ts
@@ -5,12 +5,92 @@ import * as path from 'path';
 import * as readline from 'readline';
 import * as vscode from 'vscode';
 
+const CommandID = 'unity-find-in-prefabs';
+
 const PrefabSuffix = '.prefab';
 const MetaSuffix = '.meta';
 // the regexp of match script guid in prefab file
 const PrefabScriptGUIDReg = /(?:^|\s*)m_Script\s*:\s*\{[\s\S^,]+,\s*guid\s*\:\s*([0-9a-f]{32})\s*[\s\S^\}]+\}(?:\s*|$)/i;
 // the regexp of match guid in meta file
 const MetaGUIDReg = /(?:^|\s*)guid\s*:\s*([0-9a-f]{32})(?:\s*|$)/;
+
+// whether cache has been made
+let cacheMade = false;
+// cache <script guid, the uri prefab file>
+const guidWithinPrefabs: Map<string, Set<vscode.Uri>> = new Map();
+
+/**
+ * build guid and prefab files map cache
+ */
+const buildCache = () => vscode.window.withProgress({
+    location: vscode.ProgressLocation.Window,
+    title: 'Unity: Find C# In Prefabs. Building cache',
+    cancellable: false
+}, async progress => {
+    progress.report({ increment: 0 });
+
+    // get all prefab files
+    const uris = await vscode.workspace.findFiles(`**/*${ PrefabSuffix }`, null);
+    for await (const uri of uris) {
+        try {
+            const reader = fs.createReadStream(uri.fsPath);
+            const rl = readline.createInterface({
+                input: reader,
+                crlfDelay: Infinity
+            });
+            // Note: we use the crlfDelay option to recognize all instances of CR LF
+            // ('\r\n') in input.txt as a single line break.
+            for await (const line of rl) {
+                const matcher = line.match(PrefabScriptGUIDReg);
+                if (!matcher) continue;
+
+                const guid = matcher[1];
+                if (guidWithinPrefabs.has(guid)) {
+                    guidWithinPrefabs.get(guid)!.add(uri);
+                } else {
+                    guidWithinPrefabs.set(guid, new Set([uri]));
+                }
+            }
+        } catch (err: any) {
+            console.error(`Error from ${ CommandID } when open prefab file ${ uri.fsPath }:\n${ err.message }`);
+        }
+    }
+
+    cacheMade = true;
+    progress.report({ increment: 100 });
+
+    // notify build cache complete
+    vscode.window.showInformationMessage('Build cache complete.');
+});
+
+/**
+ * get the C# file's guid
+ */
+const getGUIDOfActiveCsharpFile = async () => {
+    // mabey file not exist
+    try {
+        // read the right meta file of the C# file
+        const reader = fs.createReadStream(`${ vscode.window.activeTextEditor!.document.uri.fsPath }${ MetaSuffix }`);
+        const rl = readline.createInterface({
+            input: reader,
+            crlfDelay: Infinity
+        });
+
+        for await (const line of rl) {
+            const matcher = line.match(MetaGUIDReg);
+            if (matcher) return matcher[1];
+        }
+
+        return null;
+    } catch (err: any) {
+        console.error(`Error from ${ CommandID } when open meta file:\n${ err.message }`);
+    }
+};
+
+/**
+ * get the C# file's name
+ */
+const getFileNameOfCsharpFile = () => path.basename(vscode.window.activeTextEditor!.document.uri.fsPath);
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -20,81 +100,10 @@ export const activate = (context: vscode.ExtensionContext) => {
     // This line of code will only be executed once when your extension is activated
     console.log('Unity: Find C# In Prefabs is now active!');
 
-    // whether cache has been made
-    let cacheMade = false;
-    // cache <script guid, the uri prefab file>
-    const guidWithinPrefabs: Map<string, Set<vscode.Uri>> = new Map();
-
-    /**
-     * make guid and prefab files map cache
-     */
-    const makeCache = async () => {
-        // get all prefab files
-        const uris = await vscode.workspace.findFiles(`**/*${ PrefabSuffix }`, null);
-        for await (const uri of uris) {
-            try {
-                const reader = fs.createReadStream(uri.fsPath);
-                const rl = readline.createInterface({
-                    input: reader,
-                    crlfDelay: Infinity
-                });
-                // Note: we use the crlfDelay option to recognize all instances of CR LF
-                // ('\r\n') in input.txt as a single line break.
-                for await (const line of rl) {
-                    const matcher = line.match(PrefabScriptGUIDReg);
-                    if (!matcher) continue;
-
-                    const guid = matcher[1];
-                    if (guidWithinPrefabs.has(guid)) {
-                        guidWithinPrefabs.get(guid)!.add(uri);
-                    } else {
-                        guidWithinPrefabs.set(guid, new Set([uri]));
-                    }
-                }
-            } catch (err: any) {
-                console.error(`Error from unity-find-in-prefabs when open prefab file ${ uri.fsPath }:\n${ err.message }`);
-            }
-        }
-
-        cacheMade = true;
-
-        // notify make cache complete
-        vscode.window.showInformationMessage('Make cache complete. You can find again.');
-    };
-
-    /**
-     * get the C# file's guid
-     */
-    const getGUIDOfActiveCsharpFile = async () => {
-        // mabey file not exist
-        try {
-            // read the right meta file of the C# file
-            const reader = fs.createReadStream(`${ vscode.window.activeTextEditor!.document.uri.fsPath }${ MetaSuffix }`);
-            const rl = readline.createInterface({
-                input: reader,
-                crlfDelay: Infinity
-            });
-
-            for await (const line of rl) {
-                const matcher = line.match(MetaGUIDReg);
-                if (matcher) return matcher[1];
-            }
-
-            return null;
-        } catch (err: any) {
-            console.error(`Error from unity-find-in-prefabs when open meta file:\n${ err.message }`);
-        }
-    };
-
-    /**
-     * get the C# file's name
-     */
-    const getFileNameOfCsharpFile = () => path.basename(vscode.window.activeTextEditor!.document.uri.fsPath);
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand('unity-find-in-prefabs', async () => {
+    const disposable = vscode.commands.registerCommand(CommandID, async () => {
         // The code you place here will be executed every time your command is executed
         const { activeTextEditor } = vscode.window;
         if (!activeTextEditor) return;
@@ -107,7 +116,7 @@ export const activate = (context: vscode.ExtensionContext) => {
         }
 
         if (!cacheMade) {
-            vscode.window.showInformationMessage('Please wait for making cache.');
+            vscode.window.showInformationMessage('Please wait for building cache.');
             return;
         }
 
@@ -152,10 +161,13 @@ export const activate = (context: vscode.ExtensionContext) => {
         vscode.env.clipboard.writeText(selected.label);
     });
 
-    makeCache();
-
     context.subscriptions.push(disposable);
+
+    buildCache();
 }
 
 // this method is called when your extension is deactivated
-export const deactivate = () => { }
+export const deactivate = () => {
+    cacheMade = false;
+    guidWithinPrefabs.clear();
+};
